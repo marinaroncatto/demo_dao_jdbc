e4639892231cba4221a165d0c0f7b3baa2e9fe1c
marinaroncatto <marinam.roncatto@gmail.com>
2025-05-27 16:51:43 -0300

    DepartmentDao 1: preparando implementação

---
+++ src/application/Program2.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// Teste de Department
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		
+
+	}
+
+}
